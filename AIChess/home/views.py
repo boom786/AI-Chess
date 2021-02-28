bd415e098bd99da51e84bf65134ba7f4063e24cd
boom786 <brock4605@gmail.com>
2021-02-28 06:37:18 +0530

    Converted Original project to Django project

---
+++ AIChess/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from requests.api import request
+
+# Create your views here.
+def home(request):
+	# return HttpResponse("Hello world")
+	return render(request, 'home/index.html')
